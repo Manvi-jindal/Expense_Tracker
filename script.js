f6082c8e7ce5319777d0dc3fc525b308c566d52b
Anjali Yadav <147838692+Anjaliyadav15@users.noreply.github.com>
2024-08-17 22:03:34 +0530

    Update script.js
    Removed unwanted lines of code.

--- script.js
+++ script.js
@@ -6,12 +6,7 @@ const form = document.getElementById('form');
 const text = document.getElementById('text');
 const amount = document.getElementById('amount');
 
-// const dummyTransactions = [
-//   { id: 1, text: 'Flower', amount: -20 },
-//   { id: 2, text: 'Salary', amount: 300 },
-//   { id: 3, text: 'Book', amount: -10 },
-//   { id: 4, text: 'Camera', amount: 150 }
-// ];
+
 
 const localStorageTransactions = JSON.parse(
   localStorage.getItem('transactions')
